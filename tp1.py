f5ec83c9c3bfc3396427f7a92a6de5f90bcade57
MrLucasFischer <lucasfischerpt@gmail.com>
2018-10-03 10:54:03 +0100

    working on implementing logistic_reg

--- tp1.py
+++ tp1.py
@@ -8,24 +8,31 @@ Created on Mon Oct  1 15:51:38 2018
 """
 from helperfuncs import *
 from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.linear_model import LogisticRegression
 
 class Assignment:
-    
     def __init__(self, filename, delim):
         self.filename = filename
         self.data = read_data_file(filename, delim)
         self.data = shuffle(self.data)
 
 
-    def process_data(self, proc_type):
+    def process_data(self, proc_type = "standardize"):
+        """
+            Processes the data according to the given type of processing requested
+        """
         if (proc_type == "standardize"):
-            self.data = standardize_data(self.data[:,:-1], 0)
+            self.data[:, :-1] = standardize_data(self.data[:,:-1], 0)
         else:
-            self.data = normalize_data(self.data[:,:-1], 0)
+            self.data[:, :-1] = normalize_data(self.data[:,:-1], 0)
         
 
-    def logist_reg(self):
-        pass
+    def logistic_reg(self, folds):
+        self.process_data()
+        x_training, x_test, y_training, y_test = train_test_split(self.data, self.data[:, -1], test_size = 0.33, stratify = self.data[:, -1])
+        kfold = StratifiedKFold(n_splits = folds)
+        
     
     def knn(self):
         pass
@@ -34,4 +41,4 @@ class Assignment:
         pass
     
 assignment = Assignment("TP1-data.csv", ",")
-print(assignment.data)
+print(assignment.logistic_reg(10))
