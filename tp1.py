90f0ddeb237225983e22de624a379549aedae593
Joana Martins <joanalm@gmail.com>
2018-10-03 10:30:53 +0100

    Added process_data

--- tp1.py
+++ tp1.py
@@ -13,7 +13,15 @@ class Assignment:
     def __init__(self, filename, delim):
         self.filename = filename
         self.data = read_data_file(filename, delim)
+
+    def process_data(self, proc_type):
+        if (proc_type == "standardize"):
+            self.data = standardize_data(self.data[:,:-1], 0)
+        else:
+            self.data = normalize_data(self.data[:,:-1], 0)
+            
         
+
     def logistReg():
         pass
     
