77b1d0e0c2f5adefbb79f8723b594daf081b4efb
Joana Martins <joanalm@gmail.com>
2018-10-03 10:00:47 +0100

    Added function to read data from file

--- helperfuncs.py
+++ helperfuncs.py
@@ -6,3 +6,13 @@ Created on Wed Oct  3 09:47:38 2018
 @author: lucas
 """
 
+import numpy as np 
+
+
+def read_data_file(filename, delim):
+    """ Reads the data file and gets data separated by delimiter delim """
+    """ input: filename to read (filename), delimeter (delim) """
+    """ ouput: data """
+    # Load the data from file
+    data = np.loadtxt(filename,delimiter=delim)
+    return data
