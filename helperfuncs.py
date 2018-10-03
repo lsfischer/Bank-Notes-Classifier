5bdb822a2acbdb30636d1060fed813c12dfd5f64
Joana Martins <joanalm@gmail.com>
2018-10-03 11:03:06 +0100

    added plot cross validation error function

--- helperfuncs.py
+++ helperfuncs.py
@@ -34,8 +34,30 @@ def read_data_file(filename, delim):
     """ 
         Reads the data file and gets data separated by delimiter delim 
     """
-    """ input: filename to read (filename), delimeter (delim) """
-    """ ouput: data """
     # Load the data from file
     data = np.loadtxt(filename,delimiter=delim)
     return data
+
+
+def plot_crossVal_err(err_array, if_log_c_axis):
+    """ 
+        Plots training and cross-validation errors vs C parameter
+        if if_log_c_axis = true, C axis is displayed in log scale
+        err_array[:,0] -> C values
+        err_array[:,1] -> training errors
+        err_array[:,2] -> validation errors
+    """
+    plt.figure()
+    if (if_log_c_axis):
+        plt.plot(np.log10(err_array[:,0]),err_array[:,1],"-r", label="training")    
+        plt.plot(np.log10(err_array[:,0]),err_array[:,2],"-b", label="validation")
+        plt.xlabel('log_10(C)')
+    else:
+        plt.plot(err_array[:,0],err_array[:,1],"-r", label="training")    
+        plt.plot(err_array[:,0],err_array[:,2],"-b", label="validation")
+        plt.xlabel('C')
+    plt.ylabel('error')
+    plt.legend()
+    plt.show()
+    plt.savefig('cross_val_err_vs_c.png', dpi=300)
+    
