b2622e538bfb5a33793515f4297374c3808ea35d
MrLucasFischer <lucasfischerpt@gmail.com>
2018-10-03 10:16:04 +0100

    added structure of tp1

--- tp1.py
+++ tp1.py
@@ -6,3 +6,22 @@ Created on Mon Oct  1 15:51:38 2018
 @author: Lucas Fischer
 @author: Joana Martins
 """
+from helperfuncs import *
+
+class Assignment:
+    
+    def __init__(self, filename, delim):
+        self.filename = filename
+        self.data = read_data_file(filename, delim)
+        
+    def logistReg():
+        pass
+    
+    def knn():
+        pass
+    
+    def bayes():
+        pass
+    
+assignment = Assignment("TP1-data.csv", ",")
+print(assignment.data)
